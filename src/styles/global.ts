6771b3fed018566877f4d115cd5c55655952773c
Omar Jbara <omar.jbara2@gmail.com>
2019-06-29 20:15:56 +0200

    - Added base styled modules and some fonts and basic global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,26 @@
-import { createGlobalStyle } from "styled-components"
+import {createGlobalStyle} from "styled-components"
+import {lightGray} from 'styles/modules/colors'
+import bootstrap from 'styles/bootstrap'
 
 export default createGlobalStyle`
+  ${bootstrap};
   
+  html {
+    font-size: 100%;
+  }
+  
+  body {
+    background: ${lightGray};
+    font-family: 'Nunito', sans-serif;
+  }
+  
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 700;
+    font-family: 'Hind Siliguri', sans-serif;
+  }
 `
---
+++ src/styles/modules/colors.ts
@@ -0,0 +1,11 @@
+export const dark = '#343740'
+export const red = '#ff5c7c'
+export const lightGray = '#e8ebf3'
+export const white = '#ffffff'
+
+export default {
+  dark,
+  red,
+  lightGray,
+  white
+}
