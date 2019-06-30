9e7835db50b4425af8ab805d1d42203711da41d9
Omar Jbara <omar.jbara2@gmail.com>
2019-06-30 14:10:16 +0200

    - Added AppButton UI component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import {dark, white} from 'styles/modules/colors'
-import {rgba} from 'polished'
+import {white} from 'styles/modules/colors'
+import {MAIN_BOX_SHADOW} from 'styles/modules/variables'
 
 interface Props {
 
@@ -12,7 +12,7 @@ const CardInner = styled.div`
   padding: 30px;
   background: ${white};
   border-radius: 5px;
-  box-shadow: 0 20px 25px -5px ${rgba(dark, 0.15)},0 10px 10px -5px rgba(0,0,0,.04);
+  ${MAIN_BOX_SHADOW};
 `
 
 const Card: React.FC<Props> = (props) => {
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,12 +3,18 @@ import {FunctionComponent} from 'react'
 import styled from 'styled-components/macro'
 import Page from 'components/Page'
 import Card from 'components/ui/Card'
+import AppButton from 'components/ui/AppButton'
+import SpotifyIcon from 'components/ui/icons/Spotify'
 
 const HomeInner = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`
+
+const LoginButton = styled(AppButton)`
+
 `
 
 const Home: FunctionComponent = props => {
@@ -18,6 +24,7 @@ const Home: FunctionComponent = props => {
       <HomeInner>
         <Card>
           <h1>Home</h1>
+          <LoginButton variant="spotify" icon={SpotifyIcon}>Se connecter avec Spotify</LoginButton>
         </Card>
       </HomeInner>
     </Page>
